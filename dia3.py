8515753f46c93b9ecb0e303411985d2da769498f
thaiscarolinideoliveira-del <thaiscarolinideoliveira@gmail.com>
2026-03-04 20:02:30 -0300

    Add files via upload

---
+++ dia3.py
@@ -0,0 +1,11 @@
+# Define o valor de pi
+pi = 3.14159
+
+# Lê o valor do raio (float)
+raio = float(input())
+
+# Calcula a área
+area = pi * (raio ** 2)
+
+# Mostra o resultado com 4 casas decimais
+print(f"A={area:.4f}")
