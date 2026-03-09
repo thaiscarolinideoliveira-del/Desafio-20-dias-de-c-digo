54f10187c282e3703b176784a1c150110819c2a1
thaiscarolinideoliveira-del <thaiscarolinideoliveira@gmail.com>
2026-03-09 19:56:08 -0300

    Add files via upload

---
+++ Dia8.py
@@ -0,0 +1,10 @@
+# Programa para calcular a diferença entre produtos
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+diferenca = (A * B - C * D)
+
+print("DIFERENCA = {}".format(diferenca))
