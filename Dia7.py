f5870398e95ce0585cf2ebcc585a3af0ad6f313a
thaiscarolinideoliveira-del <thaiscarolinideoliveira@gmail.com>
2026-03-09 19:55:38 -0300

    Add files via upload

---
+++ Dia7.py
@@ -0,0 +1,9 @@
+# Programa para calcular a média ponderada de três notas
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+media = (A * 2 + B * 3 + C * 5) / 10
+
+print("MEDIA = {:.1f}".format(media))
